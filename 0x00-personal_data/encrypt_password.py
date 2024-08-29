5e8f52eaa4f22e010db1e29cbb5721b6388c77d5
AerinLom <aerinlom1@gmail.com>
2024-08-29 04:54:43 -0700

    Task 6: Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hash_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if a provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
