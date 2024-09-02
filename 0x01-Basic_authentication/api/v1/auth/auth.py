6b632e4e3dadb51d0b55951ef526c13a09018b58
AerinLom <aerinlom1@gmail.com>
2024-09-02 07:40:21 -0700

    task 4: Defined require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,20 @@ class Auth:
         """
         Determines if a given path requires authentication.
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] != '/':
+                excluded_path += '/'
+            if path == excluded_path:
+                return False
+        
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """
