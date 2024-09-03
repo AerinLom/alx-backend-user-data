f4c403e952040778a12ac401309c5ec568d8764b
AerinLom <aerinlom1@gmail.com>
2024-09-03 03:59:06 -0700

    Made changes to Task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -82,21 +82,8 @@ class BasicAuth(Auth):
         """
         Retrieves the user from a request
         """
-        auth_header = self.auth_header(request)
-        if auth_header is None:
-            return None
-
-        b64_auth = self.extract_base64_authorization_header(auth_header)
-        if b64_auth is None:
-            return None
-
+        authorization_header = self.authorization_header(request)
+        b64_auth = self.extract_base64_authorization_header(authorization_header)
         auth_token = self.decode_base64_authorization_header(b64_auth)
-        if auth_token is None:
-            return None
-
         email, password = self.extract_user_credentials(auth_token)
-        if email is None:
-            return None
-        if password is None:
-            return None
         return self.user_object_from_credentials(email, password)
