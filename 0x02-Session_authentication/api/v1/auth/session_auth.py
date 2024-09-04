6b36c3757683d87d93ce3bc34fd4b84e45ff7fa7
AerinLom <aerinlom1@gmail.com>
2024-09-04 02:56:23 -0700

    Task 1: Session Auth set up

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module for managing session authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Template class for session authentication
+    """
+    pass
