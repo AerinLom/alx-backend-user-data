44d74c7d6c86005f8ee467ab1c8cac85224b35ad
AerinLom <aerinlom1@gmail.com>
2024-09-10 04:37:39 -0700

    Task 15: User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -54,5 +54,18 @@ def logout() -> str:
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """
+    Finds a profile by session id
+    """
+    session_id = request.cookies.get("session_id")
+    found_user = AUTH.get_user_from_session_id(session_id)
+    if found_user:
+        return jsonify({"email": found_user.email})
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
