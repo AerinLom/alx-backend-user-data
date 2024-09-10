23ad3680982a336c2974dc8e8bdd1a591d4769ed
AerinLom <aerinlom1@gmail.com>
2024-09-10 05:11:24 -0700

    Task 19: Update password endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -88,12 +88,17 @@ def reset_password() -> str:
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
-    
+    success = False
+
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
+        success = True
     except ValueError:
+        success = False
+    if not success:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
